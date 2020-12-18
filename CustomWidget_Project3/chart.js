74cd78f0d78925eedf1e404bd91e0fd6bfab823a
infomotion-ilias <ilias.gountouvas@infomotion.de>
2020-12-18 13:26:45 +0100

    v3

--- CustomWidget_Project3/chart.js
+++ CustomWidget_Project3/chart.js
@@ -1,55 +1,114 @@
 (function()  {
-	let template = document.createElement("template");
-	template.innerHTML = `
+    let tmpl = document.createElement('template');
+	tmpl.innerHTML = `
+	
 
-		<form id="form">
-			<fieldset>
-				<legend>Color Properties</legend>
-				<table>
-					<tr>
-						<td>Color</td>
-						<td><input id="bps_color" type="text" size="10" maxlength="10"></td>
-					</tr>
-				</table>
-				<input type="submit" style="display:none;">
-			</fieldset>
-		</form>
-		
-		<style>
-		:host {
-			display: block;
-			padding: 1em 1em 1em 1em;
-		}
-		</style>
-	`;
-
-	class BoxBps extends HTMLElement {
-		constructor() {
-			super();
-			this._shadowRoot = this.attachShadow({mode: "open"});
-			this._shadowRoot.appendChild(template.content.cloneNode(true));
-			this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
-		}
-
-		_submit(e) {
-			e.preventDefault();
-			this.dispatchEvent(new CustomEvent("propertiesChanged", {
-					detail: {
-						properties: {
-							color: this.color
-						}
-					}
-			}));
-		}
-
-		set color(newColor) {
-			this._shadowRoot.getElementById("bps_color").value = newColor;
-		}
-
-		get color() {
-			return this._shadowRoot.getElementById("bps_color").value;
-		}
+	<!-- Styles -->
+	<style>
+	#chartdiv {
+	  width: 100%;
+	  height: 500px;
 	}
+	
+	</style>
+	
+	<!-- Resources -->
+	<script src="https://cdn.amcharts.com/lib/4/core.js"></script>
+	<script src="https://cdn.amcharts.com/lib/4/charts.js"></script>
+	<script src="https://cdn.amcharts.com/lib/4/themes/animated.js"></script>
+	
+	<!-- Chart code -->
+	<script>
+	am4core.ready(function() {
+	
+	// Themes begin
+	am4core.useTheme(am4themes_animated);
+	// Themes end
+	
+	var chart = am4core.create("chartdiv", am4charts.XYChart);
+	
+	var data = [];
+	var value = 50;
+	for(var i = 0; i < 300; i++){
+	  var date = new Date();
+	  date.setHours(0,0,0,0);
+	  date.setDate(i);
+	  value -= Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+	  data.push({date:date, value: value});
+	}
+	
+	chart.data = data;
+	
+	// Create axes
+	var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+	dateAxis.renderer.minGridDistance = 60;
+	
+	var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+	
+	// Create series
+	var series = chart.series.push(new am4charts.LineSeries());
+	series.dataFields.valueY = "value";
+	series.dataFields.dateX = "date";
+	series.tooltipText = "{value}"
+	
+	series.tooltip.pointerOrientation = "vertical";
+	
+	chart.cursor = new am4charts.XYCursor();
+	chart.cursor.snapToSeries = series;
+	chart.cursor.xAxis = dateAxis;
+	
+	//chart.scrollbarY = new am4core.Scrollbar();
+	chart.scrollbarX = new am4core.Scrollbar();
+	
+	}); // end am4core.ready()
+	</script>
+	
+	
+	<!-- HTML -->
+	<div id="chartdiv"></div>
 
-	customElements.define("com-demo-box-bps", BoxBps);
+    `;
+
+    customElements.define('com-sap-sample-helloworldtp', class HelloWorld1 extends HTMLElement {
+
+
+		constructor() {
+			super(); 
+			this._shadowRoot = this.attachShadow({mode: "open"});
+            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+		}
+
+        //Fired when the widget is added to the html DOM of the page
+        connectedCallback(){
+        }
+
+         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+        disconnectedCallback(){
+        
+        }
+
+         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
+		onCustomWidgetBeforeUpdate(oChangedProperties) {
+
+		}
+
+        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
+		onCustomWidgetAfterUpdate(oChangedProperties) {
+            this.redraw();
+        }
+        
+        //When the custom widget is removed from the canvas or the analytic application is closed
+        onCustomWidgetDestroy(){
+        }
+
+        
+        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
+        // Commented out by default
+        /*
+        onCustomWidgetResize(width, height){
+        }
+        */
+
+        redraw(){}
+    });
 })();
