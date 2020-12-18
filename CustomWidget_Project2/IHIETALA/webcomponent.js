0043db5a62b8442ed010cd7498fbc9984494ad9a
Iiro Hietala <iiro.hietala@infomotion.de>
2020-12-18 11:48:16 +0100

    hw2

--- CustomWidget_Project2/IHIETALA/webcomponent.js
+++ CustomWidget_Project2/IHIETALA/webcomponent.js
@@ -1,6 +1,7 @@
 (function()  {
     let tmpl = document.createElement('template');
-    tmpl.innerHTML = ``;
+    tmpl.innerHTML = `
+    `;
 
     customElements.define('com-sap-sample-helloworld2', class HelloWorld1 extends HTMLElement {
 
