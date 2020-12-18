ead28ed5c5cae69fad6167dbc16d72a7f63f06d8
Iiro Hietala <iiro.hietala@infomotion.de>
2020-12-18 12:10:13 +0100

    hw3

--- CustomWidget_Project2/IHIETALA/webcomponent.js
+++ CustomWidget_Project2/IHIETALA/webcomponent.js
@@ -3,20 +3,24 @@
     tmpl.innerHTML = `
     `;
 
-    customElements.define('com-sap-sample-helloworld3', class HelloWorld1 extends HTMLElement {
+    customElements.define('com-sap-sample-helloworld3', class HelloWorld extends HTMLElement {
 
 
 		constructor() {
 			super(); 
-            this._shadowRoot = this.attachShadow({mode: "open"});
+			this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._firstConnection = false;
             this._tagContainer;
             this._tagType = "h1";
-            this._tagText = "Hi, what's up, Iiro?!?";
+            this._tagText = "Hello World";
+
 		}
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
+            this._firstConnection = true;
+            this.redraw();       
         }
 
          //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
@@ -31,12 +35,24 @@
 
         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
 		onCustomWidgetAfterUpdate(oChangedProperties) {
-            this.redraw();
+            if (this._firstConnection){
+                this.redraw();
+            }
         }
         
         //When the custom widget is removed from the canvas or the analytic application is closed
         onCustomWidgetDestroy(){
+        
         }
+
+        
+        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
+        // Commented out by default
+        /*
+        onCustomWidgetResize(width, height){
+        
+        }
+        */
 
         //Getters and Setters
         get widgetText() {
@@ -46,27 +62,21 @@
         set widgetText(value) {
             this._tagText = value;
         }
-        
-        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-        // Commented out by default
-        /*
-        onCustomWidgetResize(width, height){
-        }
-        */
+        // End - Getters and Setters
 
-       redraw(){
-        if (this._tagText != null){
+        redraw(){
             if (this._tagContainer){
                 this._tagContainer.parentNode.removeChild(this._tagContainer);
             }
-    
+
             var shadow = window.getSelection(this._shadowRoot);
             this._tagContainer = document.createElement(this._tagType);
             var theText = document.createTextNode(this._tagText);    
             this._tagContainer.appendChild(theText); 
             this._shadowRoot.appendChild(this._tagContainer);
         }
-
-    }
+    
+    
     });
+        
 })();
